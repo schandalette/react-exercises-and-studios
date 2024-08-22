9de7c64e2b52b07a3e2bf369165a1e819c16e714
schandalette <schanda.lynn@gmail.com>
2024-08-21 22:40:21 -0500

    React Part 1 exercises Part1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books and More Books!";
+   let book1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIuK37gP1TPcO0tf5yh1wiK9iQwTJZWK2owQ&s";
+   let book2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQympEjXw7RdBxDOQX0UuYBXHQO4oOBPPzzLg&s";
+   let book3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3c_BEAd4j6zlx_l4Z0iJGguwI4CoRPvR5KQ&s";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
+         <img src={book1} alt="A Cat Named Balston" style={{ width: '180px', height: '250px' }} />
+         <img src={book2} alt="All Your Perfects" style={{ width: '180px', height: '250px' }} />
+         <img src={book3} alt="Silver Nitrate" style={{ width: '180px', height: '250px' }} />
+      </div>
    );
 }
