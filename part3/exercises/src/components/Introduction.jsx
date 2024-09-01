fe65dd3fe8eea36422a83a803c160370606766a1
schandalette <schanda.lynn@gmail.com>
2024-08-31 20:32:48 -0500

    class 19 exercise

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
-export default function HobbyIntroduction () {
-   return(
+export default function HobbyIntroduction() {
+   return (
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About 3D Printing</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>3D printing is older than the internet.</li>
+            <li>You can 3D print food.</li>
+            <li>3D printing can be surprisingly affordable.</li>
          </ol>
       </div>
    );
